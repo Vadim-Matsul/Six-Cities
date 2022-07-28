b9ecc4ab1fc295e17169c60d88e1ba699fe84b84
Vadim <Matsulmoscow@gmail.com>
2022-07-28 20:39:27 +0300

    Добавил Logo в отдельный компонент

--- project/src/pages/not-found-screen/not-found-screen.tsx
+++ project/src/pages/not-found-screen/not-found-screen.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../components/logo/logo';
 
 function NotFoundScreen (){
   return (
     <section className='page__main page__main--favorites page-not-found' >
-        <p>404</p>
-        <span>Page Not Found</span>
-        <Link className='page-not-found-link' to='/'>Main Page</Link>
+      <div className='header__left header__left__boost'>
+        <Logo />
+      </div>
+      <p>404</p>
+      <span>Page Not Found</span>
+      <Link className='page-not-found-link' to='/'>Main Page</Link>
     </section>
   );
 }
