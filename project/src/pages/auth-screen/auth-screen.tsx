0d38a40191392fcb1795f3a4466c676552dc3b9f
Vadim <Matsulmoscow@gmail.com>
2022-08-09 07:57:34 +0300

    Обработка авторизации
    * Сохранение JWT в storage при fulfilled

--- project/src/pages/auth-screen/auth-screen.tsx
+++ project/src/pages/auth-screen/auth-screen.tsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { FormEvent, RefObject, useRef } from 'react';
+import { useDispatch } from 'react-redux';
 import Logo from '../../components/logo/logo';
+import { AuthData, loginSession, ThunkDispatchResualt } from '../../store/actions/api-actions';
+
 
 function AuthScreen ():JSX.Element {
+  const dispatch = useDispatch() as ThunkDispatchResualt;
+  const loginRef:RefObject<HTMLInputElement> = useRef(null);
+  const passwordRef:RefObject<HTMLInputElement> = useRef(null);
+
+  function handlerSubmit (event: FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    if (loginRef.current !== null && passwordRef.current !== null){
+      const userData:AuthData = {
+        email: loginRef.current.value,
+        password: passwordRef.current.value
+      };
+      dispatch(loginSession(userData));
+    }
+  }
+
   return (
     <div className='page page--gray page--login'>
       <header className='header'>
@@ -18,14 +36,33 @@ function AuthScreen ():JSX.Element {
         <div className='page__login-container container'>
           <section className='login'>
             <h1 className='login__title'>Sign in</h1>
-            <form className='login__form form' action='#' method='post'>
+            <form
+              className='login__form form'
+              action='#'
+              method='post'
+              onSubmit={ handlerSubmit }
+            >
               <div className='login__input-wrapper form__input-wrapper'>
                 <label className='visually-hidden'>E-mail</label>
-                <input className='login__input form__input' type='email' name='email' placeholder='Email' required />
+                <input
+                  className='login__input form__input'
+                  type='email'
+                  name='email'
+                  placeholder='Email'
+                  ref={ loginRef }
+                  required
+                />
               </div>
               <div className='login__input-wrapper form__input-wrapper'>
                 <label className='visually-hidden'>Password</label>
-                <input className='login__input form__input' type='password' name='password' placeholder='Password' required />
+                <input
+                  className='login__input form__input'
+                  type='password'
+                  name='password'
+                  placeholder='Password'
+                  ref={ passwordRef }
+                  required
+                />
               </div>
               <button className='login__submit form__submit button' type='submit'>Sign in</button>
             </form>
